9003bea636e97e73851fa6226792ca73e050de4c
Dorielson Martins <dorielson.mv@gmail.com>
2025-09-25 23:14:10 -0300

    Projeto completo

---
+++ src/main/java/com/matheuslf/imc/ImcApplication.java
@@ -0,0 +1,15 @@
+package com.matheuslf.imc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// https://www.youtube.com/watch?v=4C9XSFooF88
+// https://dashboard.render.com/
+@SpringBootApplication
+public class ImcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImcApplication.class, args);
+	}
+
+}
+cd 
